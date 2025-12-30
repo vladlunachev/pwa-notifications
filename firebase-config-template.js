916e28072503e5a4faf22d4ca43e5f54d08835f2
Vlad Lunachev <v.lunachev@gmail.com>
2025-12-30 10:37:01 +0100

    Add firebase-config template for local development

---
+++ firebase-config-template.js
@@ -0,0 +1,14 @@
+// Firebase Configuration Template
+// Copy this file to 'firebase-config.js' and fill in your actual values
+// DO NOT commit firebase-config.js to git!
+
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+export const vapidKey = "YOUR_VAPID_KEY";
